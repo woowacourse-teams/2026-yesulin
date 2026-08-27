8c21185d65375a27bf59cf41a84583b2c7f18b1f
jihwankim128 <jihwan1535@nate.com>
2026-08-27 18:15:05 +0900

    test: 테스트 환경 메모리 안정화

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    maxHeapSize = "1g"
     systemProperty("spring.profiles.active", "test")
     systemProperty("spring.flyway.locations", "classpath:db/migration")
 }
